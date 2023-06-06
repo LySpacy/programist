c3855321575e87bd4a40410196ed181af292f505
Vladimir <117316977+LySpacy@users.noreply.github.com>
2023-06-06 10:35:09 +0300

    Add files via upload

---
+++ 14/14/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace _14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(204, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите дату";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(107, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "День";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(278, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Месяц";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(483, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Год";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31"});
+            this.comboBox1.Location = new System.Drawing.Point(75, 104);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(111, 37);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.TabStop = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Январь",
+            "Февраль",
+            "Март",
+            "Апрель",
+            "Май",
+            "Июнь",
+            "Июль",
+            "Август",
+            "Сентябрь",
+            "Октябрь",
+            "Ноябрь ",
+            "Декабрь"});
+            this.comboBox2.Location = new System.Drawing.Point(210, 103);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(179, 37);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Январь",
+            "Февраль",
+            "Март",
+            "Апрель",
+            "Май",
+            "Июнь",
+            "Июль",
+            "Август",
+            "Сентябрь",
+            "Октябрь",
+            "Ноябрь ",
+            "Декабрь"});
+            this.comboBox3.Location = new System.Drawing.Point(431, 103);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(154, 37);
+            this.comboBox3.TabIndex = 9;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(153, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(368, 44);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Узнать день недели";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(287, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 31);
+            this.label5.TabIndex = 11;
+            this.label5.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 269);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 14/14/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Calculate(string years, string mounth, string day)
+        {
+            int m = 0;
+            switch (mounth)
+            {
+                case "Январь": m = 1; break;
+                case "Февраль": m = 2;  break;
+                case "Март": m = 3; break;
+                case "Апрель": m = 4; break;
+                case "Май": m = 5; break;
+                case "Июнь": m = 6; break;
+                case "Июль": m = 7; break;
+                case "Август": m = 8; break;
+                case "Сентябрь": m = 9; break;
+                case "Октябрь": m = 10; break;
+                case "Ноябрь": m = 11; break;
+                case "Декабрь": m = 12; break;
+            }
+            int a = (14 - m) / 12;
+            int Y = Convert.ToInt32(years) - a;
+            int M = m + 12 * a - 2;
+            int N = (7000 + Convert.ToInt32(day) + Y + (Y / 4) - (Y / 100) + (Y / 400) + ((31 * M) / 12)) % 7;
+            switch (N)
+            {
+                case 0: label5.Text = "Воскресенье"; break;
+                case 1: label5.Text = "Понедельник"; break;
+                case 2: label5.Text = "Вторник"; break;
+                case 3: label5.Text = "Среда"; break;
+                case 4: label5.Text = "Четверг"; break;
+                case 5: label5.Text = "Пятница"; break;
+                case 6: label5.Text = "Суббота"; break;
+            }
+        }
+        static void reprit28(ComboBox cb)
+        {
+            cb.Text = "";
+            cb.Items.Clear();
+            for (int i = 1; i < 29; i++)
+            {
+                cb.Items.Add(i.ToString());
+            }
+        }
+        static void reprit29(ComboBox cb)
+        {
+            cb.Text = "";
+            cb.Items.Clear();
+            for (int i = 1; i < 30; i++)
+            {
+                cb.Items.Add(i.ToString());
+            }
+        }
+
+        static void reprit30(ComboBox cb)
+        {
+            cb.Text = "";
+            cb.Items.Clear();
+            for (int i = 1; i < 31; i++)
+            {
+                cb.Items.Add(i.ToString());
+            }
+        }
+     
+        static bool checkYears(string a)
+        {
+            int years = Convert.ToInt32(a);
+
+            if (a == null) years = 0;
+                if ((years % 4 == 0 && years % 100 != 0) || years % 400 == 0) return true;
+            else return false;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox3.Items.Clear();
+            for (int i = 1583; i < 3201; i++)
+            {
+                comboBox3.Items.Add(i);
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox2.Text)
+            {
+
+                case "Январь": break;
+                case "Март": break;
+                case "Май": break;
+                case "Июль": break;
+                case "Август": break;
+                case "Октябрь": break;
+                case "Декабрь": break;
+                case "Апрель":
+                case "Июнь":
+                case "Сентябь":
+                case "Ноябрь": reprit30(comboBox1); break;
+                case "Февраль":
+                    if (comboBox3.Text != String.Empty)
+                    if (checkYears(comboBox3.Text))
+                    {
+                        reprit29(comboBox1);
+                    }
+                    else
+                    {   
+                        reprit28(comboBox1);
+                    }
+                    break;
+
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox2.Text)
+            {
+
+                case "Январь": break;
+                case "Март": break;
+                case "Май": break;
+                case "Июль": break;
+                case "Август": break;
+                case "Октябрь": break;
+                case "Декабрь": break;
+                case "Апрель":
+                case "Июнь":
+                case "Сентябь":
+                case "Ноябрь": reprit30(comboBox1); break;
+                case "Февраль":
+                    if (checkYears(comboBox3.Text))
+                    {
+                        reprit29(comboBox1);
+                    }
+                    else
+                    {
+                        reprit28(comboBox1);
+                    }
+                    break;
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label5.Text = "";
+            label5.Visible = true;
+            if (comboBox1.Text == String.Empty || comboBox2.Text == String.Empty || comboBox3.Text == String.Empty)
+            {
+                label5.Text = "Выберите дату";
+                label5.ForeColor = Color.Red;
+            }
+            else Calculate(comboBox3.Text, comboBox2.Text, comboBox1.Text);
+        }
+    }
+}
