f27b6a26e31ce9ac16647339043b725a4dfc879f
Vladimir <117316977+LySpacy@users.noreply.github.com>
2023-06-06 21:49:20 +0300

    Add files via upload

---
+++ 22/22/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace _22
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(4, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Квадратная матрица n X n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "n = ";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(53, 40);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(48, 26);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(43, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 60);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Сгенерировать матрицу";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(32, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(249, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Генерация невозможна";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(327, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(220, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Выберите диагональ";
+            this.label4.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Главная",
+            "Вспомогательная"});
+            this.comboBox1.Location = new System.Drawing.Point(553, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(213, 33);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Visible = false;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(315, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(614, 480);
+            this.panel1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(9, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(279, 83);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Заполнить случайными значениями";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(781, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(136, 50);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Проверить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(13, 204);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 24);
+            this.label5.TabIndex = 10;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(411, 42);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(296, 24);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Заполните все ячейки матрицы";
+            this.label6.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(298, 152);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ 22/22/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _22
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int[,] turn(int[,] arr)
+        {
+            int[,] arr2 = new int[arr.GetLength(1), arr.GetLength(0)];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr2[j, i] = arr[i, arr.GetLength(1) - 1 - j];
+                }
+            }
+
+            return arr2;
+        }
+
+        static bool checkMatrix(int[,] arr)
+        {
+            bool status = true;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1) - i; j++)
+                {
+                    if (arr[i,j] != arr[arr.GetLength(0)-1-j, arr.GetLength(1)-1-i]) status = false;
+                    if (status == false) break;
+                }
+                if (status == false) break;
+            }
+            return status;
+        }
+
+        TextBox[,] tb;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Visible = false;
+            label4.Visible = false;
+            comboBox1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            label6.Visible = false;
+
+            panel1.Controls.Clear();
+           
+            int n = Convert.ToInt32(numericUpDown1.Value);
+
+            if (n < 1)
+            {
+                label3.Visible = true;
+
+                this.Width = 320;
+                this.Height = 225;
+
+            }
+            else
+            {
+                panel1.Height = n * 100;
+                panel1.Width = n * 100;
+
+                label4.Visible = true;
+                comboBox1.Visible = true;
+                button2.Visible = true;
+                button3.Visible = true;
+
+                this.Width = 1000;
+                this.Height = 500;
+
+                tb = new TextBox[n, n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        tb[i, j] = new System.Windows.Forms.TextBox();
+                        tb[i, j].Location = new System.Drawing.Point(20 + j * 60, 20 + i * 28);
+                        tb[i, j].Name = "textBox" + (i + 1).ToString();
+                        tb[i, j].Size = new System.Drawing.Size(60, 40);
+                        tb[i, j].Font = new Font("Microsoft Sans Serif", 14);
+                        tb[i, j].TabIndex = i + 1;
+                        panel1.Controls.Add(tb[i, j]);
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label5.Visible = false;
+            label6.Visible = false;
+            Random random = new Random();
+            for (int i = 0; i < tb.GetLength(0); i++)
+            {
+                for (int j = 0; j < tb.GetLength(0); j++)
+                {
+                    tb[i, j].Text = random.Next(-10, 11).ToString();                   
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label6.Visible = false;
+            label5.Visible = true;
+
+            int[,] arr = new int[tb.GetLength(0), tb.GetLength(0)];
+            bool check = true;
+            for (int i = 0; i < tb.GetLength(0); i++)
+            {
+                for (int j = 0; j < tb.GetLength(0); j++)
+                {
+                    if (int.TryParse(tb[i, j].Text, out arr[i, j]) && tb[i, j].Text != String.Empty) { }
+                    else check = false;
+                }
+            }
+
+            if (check)
+            {
+                for (int i = 0; i < tb.GetLength(0); i++)
+                {
+                    for (int j = 0; j < tb.GetLength(0); j++)
+                    {
+                        if (tb[i, j].Text == String.Empty) { }
+                    }
+                }
+
+                if (comboBox1.Text == String.Empty)
+                {
+                    label6.Visible = true;
+                    label6.Text = "Выберите диагональ, для проверки";
+                    label6.ForeColor = Color.Red;
+                }
+                else if (comboBox1.Text == "Главная")
+                {
+                    arr = turn(arr);
+                    if (checkMatrix(arr))
+                    {
+                        label5.Text = "Матрица \nсимметрична относительно \nглавной диагонали";
+                        label5.ForeColor = Color.Green;
+                    }
+                    else
+                    {
+                        label5.Text = "Матрица \nне симметрична относительно \nглавной диагонали";
+                        label5.ForeColor = Color.Purple;
+                    }
+                }
+                else
+                {
+                    if (checkMatrix(arr))
+                    {
+                        label5.Text = "Матрица \nсимметрична относительно \nвспомогательной диагонали";
+                        label5.ForeColor = Color.Green;
+                    }
+                    else
+                    {
+                        label5.Text = "Матрица \nне симметрична относительно \nвспомогательной диагонали";
+                        label5.ForeColor = Color.Purple;
+                    }
+                }
+            }
+            else label6.Visible = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {          
+            if (comboBox1.Text == "Главная")
+            {
+                for (int i = 0; i < tb.GetLength(0); i++)
+                {
+                    for (int j = 0; j < tb.GetLength(0); j++)
+                    {
+                        if (i == j) tb[i,j].BackColor = Color.Green;
+                        if ((i == tb.GetLength(0) - 1 - j || j == tb.GetLength(0) - 1 - i) && i != j) tb[i, j].BackColor = Color.White;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < tb.GetLength(0); i++)
+                {
+                    for (int j = 0; j < tb.GetLength(0); j++)
+                    {
+                        if (i == tb.GetLength(0)-1-j || j == tb.GetLength(0) - 1 - i) tb[i, j].BackColor = Color.Green;
+                        if (i == j && i != tb.GetLength(0) - 1 - j || j != tb.GetLength(0) - 1 - i) tb[i, j].BackColor = Color.White;
+                    }
+                }
+            }
+        }
+    }
+}
